1f3d7030649e1dda789aca926a47b608ec1f90e4
FullStackMan <allan.latruffe@gmail.com>
2023-07-25 17:10:29 +0200

    ajout des links

--- portefolio-allan-latruffe/src/pages/home/home.jsx
+++ portefolio-allan-latruffe/src/pages/home/home.jsx
@@ -29,7 +29,7 @@ export default function Home() {
                     <p className="has-text-centered mb-5 has-text-light"> Si vous avez des questions, des opportunités de collaboration ou simplement envie de discuter, n'hésitez pas à me contacter. Je serais ravi d'échanger avec vous et d'en apprendre davantage sur vos projets.</p>
                     <div className="buttons is-centered">
                         <a className="button is-primary has-text-light" href="http://linkedin.com/in/allan-latruffe">Linkedin</a>
-                        <button className="button is-secondary">Contact</button>
+                        <a className="button is-secondary" href="/contact">Contact</a>
                     </div>
 
                 </Hero>
