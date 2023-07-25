5048bd046fe5ef15679a5b8b45c2ce9ebf8006b2
FullStackMan <allan.latruffe@gmail.com>
2023-07-25 16:15:07 +0200

    Chnagement de couleur

--- portefolio-allan-latruffe/src/components/hero/hero.jsx
+++ portefolio-allan-latruffe/src/components/hero/hero.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 
+import './hero.css';
+
 export default function Hero(props) {
     const { children } = props;
     return (
-        <div className="hero is-info is-medium">
+        <div className="hero is-blue is-medium">
             <div className="hero-body">
                 <div className="description">
                     {children}
--- portefolio-allan-latruffe/src/pages/home/home.jsx
+++ portefolio-allan-latruffe/src/pages/home/home.jsx
@@ -9,12 +9,12 @@ export default function Home() {
         <>
             <main>
                 <Hero> 
-                <h3 className="title is-is-family-sans-serif">Hello !</h3>
-                <p>
+                <h3 className="title is-is-family-sans-serif has-text-light">Hello !</h3>
+                <p className="has-text-light">
                 Je suis un développeur passionné avec 6 ans d'expérience en tant que freelance, spécialisé dans le développement web avec React. J'ai également été webmaster pendant plusieurs années et j'ai occupé un poste de direction de projet dans une PME. 
                 </p>
                 &nbsp;
-                <p>
+                <p className="has-text-light">
                 Polyvalent, je suis à l'aise aussi bien en front-end qu'en back-end. J'ai acquis de solides compétences techniques, mais aussi des soft skills essentielles, telles que la communication et la gestion de projet. Toujours avide d'apprendre et de relever de nouveaux défis, je suis à la recherche de projets stimulants où je pourrai mettre à profit mon expertise pour créer des solutions web innovantes.
                 </p>
 
@@ -25,10 +25,10 @@ export default function Home() {
                 <Carousel />
 
                 <Hero>
-                    <h3 className="title is-is-family-sans-serif has-text-centered">Prenons Contact</h3>
-                    <p className="has-text-centered mb-5"> Si vous avez des questions, des opportunités de collaboration ou simplement envie de discuter, n'hésitez pas à me contacter. Je serais ravi d'échanger avec vous et d'en apprendre davantage sur vos projets.</p>
+                    <h3 className="title is-is-family-sans-serif has-text-centered has-text-light">Prenons Contact</h3>
+                    <p className="has-text-centered mb-5 has-text-light"> Si vous avez des questions, des opportunités de collaboration ou simplement envie de discuter, n'hésitez pas à me contacter. Je serais ravi d'échanger avec vous et d'en apprendre davantage sur vos projets.</p>
                     <div className="buttons is-centered">
-                        <a className="button is-success" href="http://linkedin.com/in/allan-latruffe">Linkedin</a>
+                        <a className="button is-primary has-text-light" href="http://linkedin.com/in/allan-latruffe">Linkedin</a>
                         <button className="button is-secondary">Contact</button>
                     </div>
 
